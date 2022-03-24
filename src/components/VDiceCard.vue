87dc8ab5a69e5c067f89776f398c85b730bb2e43
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-24 12:00:31 +0100

    feat: Create function that rolls a d20
    The function that rolls a d20 has been created

--- src/components/VDiceCard.vue
+++ src/components/VDiceCard.vue
@@ -1,4 +1,30 @@
-<script setup></script>
+<script setup>
+let rollResult = $ref("");
+const props = defineProps({
+  diceType: {
+    type: String,
+    default: "D20",
+  },
+});
+
+function rollDie(diceType) {
+  if (!diceType) return;
+  if (diceType === "D20") {
+    let dieResult = rolld20();
+
+    return (rollResult = dieResult);
+  }
+}
+
+function rolld20() {
+  const diceValue = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  let dieResult = diceValue[Math.floor(Math.random() * diceValue.length)];
+  console.log(dieResult);
+  return dieResult;
+}
+</script>
 
 <template>
   <div class="card w-96 bg-base-100 shadow-xl">
@@ -10,10 +36,12 @@
       />
     </figure>
     <div class="card-body items-center text-center">
-      <h2 class="card-title">Dice:</h2>
-      <p class="font-bold">Result: <span></span></p>
+      <h2 class="card-title">Dice: {{ diceType }}</h2>
+      <p class="font-bold">
+        Result: <span>{{ rollResult }}</span>
+      </p>
       <div class="card-actions">
-        <button class="btn btn-primary">Roll</button>
+        <button class="btn btn-primary" @click="rollDie(diceType)">Roll</button>
       </div>
     </div>
   </div>
--- src/components/VDiceView.vue
+++ src/components/VDiceView.vue
@@ -2,7 +2,7 @@
 import VDiceCard from "./VDiceCard.vue";
 import VSelectDice from "./VSelectDice.vue";
 
-let diceType = $ref("");
+let diceType = $ref("D20");
 
 function setDiceType(value) {
   return (diceType = value);
@@ -14,7 +14,7 @@ function setDiceType(value) {
     <div class="hero-content text-center flex flex-col">
       <h1 class="text-4xl">Dice Generator</h1>
       <VSelectDice @emit-dice-type="setDiceType" />
-      <VDiceCard />
+      <VDiceCard :dice-type="diceType" />
     </div>
   </div>
 </template>
