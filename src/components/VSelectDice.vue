fb9523ec557de3cb865e44475ecfdd3705294769
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-28 16:01:09 +0200

    refactor: Change order of dice selection types
    The order of the dice have been changed in the select element

--- src/components/VSelectDice.vue
+++ src/components/VSelectDice.vue
@@ -10,11 +10,11 @@ let diceType = $ref("Pick your dice");
     @change="$emit('emitDiceType', diceType)"
   >
     <option disabled selected>Pick your dice</option>
-    <option value="D4">D4</option>
-    <option value="D6">D6</option>
-    <option value="D8">D8</option>
-    <option value="D10">D10</option>
-    <option value="D12">D12</option>
     <option value="D20">D20</option>
+    <option value="D12">D12</option>
+    <option value="D10">D10</option>
+    <option value="D8">D8</option>
+    <option value="D6">D6</option>
+    <option value="D4">D4</option>
   </select>
 </template>
