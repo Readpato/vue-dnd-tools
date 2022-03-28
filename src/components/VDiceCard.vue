38eb09876f0b0366e4123bd42e143cd6c6055c2a
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-28 16:10:12 +0200

    feat: Add Natural 20 and Natural 1 messages
    Now when the user rolls a Natural 20 or a Natural 1 with a D20 a special message will appear

--- src/components/VDiceCard.vue
+++ src/components/VDiceCard.vue
@@ -70,7 +70,13 @@ function rolld20() {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
   let dieResult = diceValue[Math.floor(Math.random() * diceValue.length)];
-  return dieResult;
+  if (dieResult === 20) {
+    return "Natural 20!";
+  } else if (dieResult === 1) {
+    return "Oh... Natural 1!";
+  } else {
+    return dieResult;
+  }
 }
 function rolld12() {
   setLastRoll();
