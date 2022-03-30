9d6056101632a9eb0f4d04956c7c94c56b1c490a
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-30 19:25:52 +0200

    test: Create E2E test for d20 die
    A test has been created to assess that the D20 die has been created correctly

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -60,5 +60,54 @@ context("D&D Tools", () => {
         .and("contain.text", "D4")
         .and("contain.value", "D4");
     });
+    it("Change die type, roll it and assess the value is stored", () => {
+      let firstRoll = [];
+      cy.get("select").select("D20");
+      cy.get(".d20-svg").should("be.visible");
+      cy.get(".card-body > h2").should("contain.text", "Dice: D20");
+      cy.get(".card-body > p:last").should(
+        "contain.text",
+        "Result: Not rolled yet!"
+      );
+      cy.get("button").click();
+      cy.get(".card-body > p:last").should(
+        "not.contain.text",
+        "Result: Not rolled yet!"
+      );
+      cy.get(".card-body > p:last").then((pElement) => {
+        if (pElement[0].textContent.trim() === "Result: Natural 20!") {
+          return firstRoll.push(
+            pElement[0].textContent.split("Result: Natural ")[1].split("!")[0]
+          );
+        } else if (
+          pElement[0].textContent.trim() === "Result: Oh no... Natural 1!"
+        ) {
+          return firstRoll.push(
+            pElement[0].textContent.split("Oh no... Natural ")[1].split("!")[0]
+          );
+        } else {
+          return firstRoll.push(pElement[0].textContent.split(" ")[2]);
+        }
+      });
+      cy.get("button").click();
+      cy.get(".card-body > p:first").then((pElement) => {
+        if (pElement[0].textContent.trim() === "Last result: Natural 20!") {
+          let secondRoll = pElement[0].textContent
+            .split("Last result: Natural ")[1]
+            .split("!")[0];
+          return expect(firstRoll[0]).to.equal(secondRoll);
+        } else if (
+          pElement[0].textContent.trim() === "Last result: Oh no...  Natural 1!"
+        ) {
+          let secondRoll = pElement[0].textContent
+            .split("Last result: Natural ")[1]
+            .split("!")[0];
+          return expect(firstRoll[0]).to.equal(secondRoll);
+        } else {
+          let secondRoll = pElement[0].textContent.split("Last result: ")[1];
+          return expect(firstRoll[0]).to.equal(secondRoll);
+        }
+      });
+    });
   });
 });
