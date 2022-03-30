bf0322ac9667811fb33c917daa301c0236b57313
Readpato <87768087+Readpato@users.noreply.github.com>
2022-03-30 16:34:25 +0200

    test: Create E2E test for select element
    The E2E test that assures that the select element has been rendered correctly has been created with Cypress

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -20,4 +20,45 @@ context("D&D Tools", () => {
         .and("contain.text", "Spell Encyclopedia");
     });
   });
+
+  describe("Dice Generator", () => {
+    it("Assess that the h1 element has been rendered correctly", () => {
+      cy.get("h1").should("contain.text", "Dice Generator");
+    });
+    it("Makes sure the select element has been rendered correctly", () => {
+      cy.get("select").should("contain.text", "Pick your dice");
+      cy.get("select option").should("have.length", "7");
+      cy.get("select option").eq(0).should("be.disabled");
+      cy.get("select option")
+        .eq(1)
+        .should("be.enabled")
+        .and("contain.text", "D20")
+        .and("contain.value", "D20");
+      cy.get("select option")
+        .eq(2)
+        .should("be.enabled")
+        .and("contain.text", "D12")
+        .and("contain.value", "D12");
+      cy.get("select option")
+        .eq(3)
+        .should("be.enabled")
+        .and("contain.text", "D10")
+        .and("contain.value", "D10");
+      cy.get("select option")
+        .eq(4)
+        .should("be.enabled")
+        .and("contain.text", "D8")
+        .and("contain.value", "D8");
+      cy.get("select option")
+        .eq(5)
+        .should("be.enabled")
+        .and("contain.text", "D6")
+        .and("contain.value", "D6");
+      cy.get("select option")
+        .eq(6)
+        .should("be.enabled")
+        .and("contain.text", "D4")
+        .and("contain.value", "D4");
+    });
+  });
 });
